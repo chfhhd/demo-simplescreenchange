486ab07566cd500d8c9db5de44980432d94fceaa
Keanu Grespan <keanu.grespan@stud.hochschule-heidelberg.de>
2023-12-14 12:01:22 +0100

    Names of state_game_init und state_menu_init vertauscht

--- src/state_game.cpp
+++ src/state_game.cpp
@@ -1,9 +1,16 @@
 #include "state_game.h"
-
+#include "raylib.h"
 #include "state.h"
 
-void state_game_init() {
+Vector2 enemies[50];
+Texture2D texture;
 
+void state_game_init() {
+    for (int i = 0; i < 50; i++) {
+        enemies[i].x = GetRandomValue(0, GetScreenWidth() - texture.width);
+        enemies[i].y = GetRandomValue(0, GetScreenHeight() - texture.height);
+    }
+    texture = LoadTexture("assets/graphics/testimage.png");
 }
 
 void state_game_update() {
@@ -18,13 +25,18 @@ void state_game_draw() {
 
     ClearBackground(WHITE);
 
-    const char *text = "This is the game screen";
+    for (Vector2 element: enemies) {
+        DrawTexture(texture, (int) element.x, (int) element.y, WHITE);
+    }
+
+    /*const char *text = "This is the game screen";
     int fontSize = 30;
+
 
     float textWidth = MeasureText(text, fontSize);
     float centerX = (GetScreenWidth() - textWidth) / 2;
 
-    DrawText(text, centerX, 10, fontSize, LIGHTGRAY);
+    DrawText(text, centerX, 10, fontSize, LIGHTGRAY);*/
 
     EndDrawing();
 }
